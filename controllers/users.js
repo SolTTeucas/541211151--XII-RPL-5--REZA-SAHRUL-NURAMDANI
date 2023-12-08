2ae8d08266795f7e0a6dcc8c7caf6e5e1685debb
SolTTeucas <rezasahrull88@gmail.com>
2023-12-09 00:26:58 +0700

    second commit

---
+++ controllers/users.js
@@ -0,0 +1,70 @@
+const User = require('../models/user')
+
+module.exports ={
+  index: async(req, res) => {
+    try {
+      const users = await User.find()
+      if (users.length > 0){
+        res.status(200).json({
+          status: true,
+          data:users,
+          method:req.method,
+          url:req.url
+        })
+      }else{
+        res.json({
+          status: false,
+          message: "Data masih kosong"
+        })
+      }
+    } catch (error) {
+      res.status(400).json({success: false})
+    }
+      
+      },
+    store: async (req, res) => {
+      try {
+        const user= await User.create(req.body)
+        res.status(200).json({
+          status: true,
+          data:user,
+          method:req.method,
+          url:req.url,
+          message: "Data berhasil ditambahkan"
+        })
+      } catch (error) {
+        
+      }
+        users.push(req.body)
+        
+      },
+      update:(req, res) => {
+        const id = req.params.id
+        users.filter(user => {
+          if(user.id==id){
+            user.nama = req.body.nama
+            user.email = req.body.email
+            return user
+          }
+        })
+        res.json({
+          status: true,
+          data:users,
+          method:req.method,
+          url:req.url,
+          message: "Data berhasil diubah"
+        })
+      },
+      delete:(req, res) => {
+        const id = req.params.id
+        users = users.filter(user => user.id != id)
+    
+        res.json({
+          status: true,
+          data:users,
+          method:req.method,
+          url:req.url,
+          message: "Data berhasil dihapus"
+        })
+      }
+}
